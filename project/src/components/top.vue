c6bcc4c9c8897a5e6d875bd4a305d185208078ce
Dorian <doriangueguen@orange.fr>
2018-05-14 11:21:48 +0200

    title header

---
+++ project/src/components/top.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <h1 class="mainTitle"> {{ mainTitle }}</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      mainTitle: 'Keyboard Hero made by Dorian & Pierre'
+    }
+  }
+}
+</script>
+
+<style scoped lang="css">
+.mainTitle {
+  font-size: 20px;
+  color: red;
+  background-color: #686868;
+}
+</style>
